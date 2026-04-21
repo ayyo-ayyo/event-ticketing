f6e62a98822009615617e62872fae1726beb0f03
luckyy838 <hlkovvuri@gmail.com>
2026-04-21 04:52:54 -0400

    made payment service idempotent

--- payment-service/src/index.js
+++ payment-service/src/index.js
@@ -1,9 +1,22 @@
 const express = require("express");
 
+const redis = require("redis");
+
+
+
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 3003;
+
+const redisClient = redis.createClient({
+  url: process.env.REDIS_URL || "redis://localhost:6379"
+});
+
+redisClient.on("error", (err) => console.error("Redis error:", err));
+
+
+
 
 app.get("/health", async (req, res) => {
   res.status(200).json({
@@ -22,25 +35,41 @@ app.post("/payments", async (req, res) => {
     });
   }
 
+  const existing = await redisClient.get(`payment:${purchaseId}`);
+  if(existing){
+    return res.status(200).json(JSON.parse(existing));
+  }
   
   const success = Math.random() > 0.1;
 
-  if (success) {
-    return res.status(200).json({
-      status: "success",
-      purchaseId,
-      message: "Payment processed",
-    });
-  }
 
-  return res.status(402).json({
-    status: "failed",
-    purchaseId,
-    message: "Payment declined",
-  });
+  const result = success? {status: "success", purchaseId, message: "Payment processed"} : { status: "failed", purchaseId, message: "Payment declined"};
+  const statusCode = success ? 200 : 402
+  
+  await redisClient.set(`payment:${purchaseId}`, JSON.stringify(result));
+  
+  return res.status(statusCode).json(result)
+
+
+  // if (success) {
+  //   return res.status(200).json({
+  //     status: "success",
+  //     purchaseId,
+  //     message: "Payment processed",
+  //   });
+  // }
+
+
+  // return res.status(402).json({
+  //   status: "failed",
+  //   purchaseId,
+  //   message: "Payment declined",
+  // });
 });
 
 async function startServer() {
+  await redisClient.connect();
+
   app.listen(PORT, () => {
     console.log(`Payment Service listening on port ${PORT}`);
   });
